ef355730a0943e7095dbc3ccbb9b9265cfd27c30
MarshallRawson <marshallrawson@ufl.edu>
2020-05-05 13:40:54 -0400

    LUNG_SIM: Threading Fix

--- code/simulation/core.py
+++ code/simulation/core.py
@@ -2,7 +2,7 @@
 from ctypes import *
 
 class Core(object):
-  def __init__(self, update_semaphore, sensor_data_queue, command_queue):
+  def __init__(self, update_semaphore, sensor_data, command):
     core_so = "../objects/libcore.so"
 
     core_main = CDLL(core_so)
@@ -10,6 +10,7 @@ class Core(object):
     print (type(core_main))
     core_main.main()
   #TODO: add the hardware interfaces here that will be called from C
+
   # Temaplate for hardware interface get data function
   #  wait for time update semaphore
   #  get data from the sensore data queue
@@ -22,5 +23,5 @@ class Core(object):
 
 
   @classmethod
-  def spin(cls, **kwargs):
-    core = Core(kwargs)
+  def spin(cls, *args):
+    core = Core(*args)
--- code/simulation/simulate.py
+++ code/simulation/simulate.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
-from core import Core
 import threading
+import sys
 from queue import Queue
 
+from core import Core
 
 
 if __name__ == "__main__":
   print('starting sim')
   # who ever has the update semaphore / mutex gets to mess with the queues
-  update_seamphore = threading.Semaphore(1)
+  update_semaphore = threading.Semaphore(1)
   sensor_data = Queue()
   command = Queue()
 
-  core_thread = threading.Thread(target=Core.spin(), args=(update_semaphore, sensor_data, command))
+  core_thread = threading.Thread(target=Core.spin,
+                  args=(update_semaphore, sensor_data, command))
+  core_thread.start()
+  try:
+    while True:
+      pass
+      # wait for the update semaphore
+      # read the command queue
+      # calculate the next time step values appropriately
+      # put the appropriate data in the command queue
+      # release the update semaphore
+  except KeyboardInterrupt as e:
+    core_thread.stop()
+    sys.exit(0)
+
+
+
+
