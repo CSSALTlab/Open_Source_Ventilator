bb4f71d970c36de1b67f0f0c7444c7165916229e
MarshallRawson <marshallrawson@ufl.edu>
2020-05-06 00:24:07 -0400

    LUNG_SIM: Added some output functions

--- code/include/hardware_interface.h
+++ code/include/hardware_interface.h
@@ -1,9 +1,17 @@
 #pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
 //prototype the hardware interface functions here
 
 void initialize_hardware();
 
 void uart_print(char* str);
 
+void lcd_print(uint8_t row, uint8_t col, const char* string);
 
+void sound_alarm();
 
+void silence_alarm();
+
+void delay_ms(uint32_t ms);
